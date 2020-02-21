ea405f85471217ed6a8abf88e350fc694cf497af
Pravin <pra.durugkar@gmail.com>
2020-02-21 10:49:56 +0530

    implemented the router

--- src/App.js
+++ src/App.js
@@ -1,19 +1,35 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+//import BrowserRouter, {Route, Link, Switch} from 'react-router-dom';
+//import { BrowserRouter, Route, Link, Switch } from 'react-router-dom'
+
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import Home from './pages/Home';
+import About from './pages/About';
+import Contact from './pages/Contact';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <Router>
+        <div>
+          <h2>Welcome to React Router Tutorial</h2>
+          <nav className="navbar navbar-expand-lg navbar-light bg-light">
+          <ul className="navbar-nav mr-auto">
+            <li><Link to={'/'} className="nav-link"> Home </Link></li>
+            <li><Link to={'/contact'} className="nav-link">Contact</Link></li>
+            <li><Link to={'/about'} className="nav-link">About</Link></li>
+          </ul>
+          </nav>
+          <hr />
+          <Switch>
+              <Route exact path='/' component={Home} />
+              <Route path='/contact' component={Contact} />
+              <Route path='/about' component={About} />
+          </Switch>
+        </div>
+      </Router>
     );
   }
 }
---
+++ src/pages/Contact.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+function Contact() {
+	return (
+		<div className="home-page">Contact</div>
+	)
+}
+export default Contact;
